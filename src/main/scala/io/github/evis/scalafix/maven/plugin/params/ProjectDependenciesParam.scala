bc7459ceff193315ec063cd3d5d026fd23e872de
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2021-06-28 21:53:21 -0700

    ProjectDependenciesParam: use only zip artifacts

--- src/main/scala/io/github/evis/scalafix/maven/plugin/params/ProjectDependenciesParam.scala
+++ src/main/scala/io/github/evis/scalafix/maven/plugin/params/ProjectDependenciesParam.scala
@@ -1,5 +1,9 @@
 package io.github.evis.scalafix.maven.plugin.params
 
+import java.util.jar.JarFile
+
+import scala.util.Try
+
 import org.apache.maven.artifact.Artifact
 
 object ProjectDependenciesParam {
@@ -10,7 +14,13 @@ object ProjectDependenciesParam {
       deps.find { dep =>
         dep.getGroupId == "org.scala-lang" && dep.getArtifactId == "scala-library"
       }
-    _.withClasspath(deps.map(_.getFile.toPath))
-      .withScalaVersion(scalaLibrary.map(_.getVersion))
+    _.withClasspath(deps.flatMap { dep =>
+      /* Some dependencies could be POM files, which scalameta's ClassPathIndex
+       * (used by scalafix) would fail to load since it tries to open them using
+       * JarFile. Hence, use JarFile ourselves to filter. */
+      val file = dep.getFile
+      val ok = !file.isFile || Try(new JarFile(file)).isSuccess
+      if (ok) Some(file.toPath) else None
+    }).withScalaVersion(scalaLibrary.map(_.getVersion))
   }
 }
