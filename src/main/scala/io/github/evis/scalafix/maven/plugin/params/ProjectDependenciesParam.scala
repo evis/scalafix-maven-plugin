ae02ae105101701a0acd4bf1ce193f3483e475a6
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2021-06-28 21:53:21 -0700

    ProjectDependenciesParam: use only zip artifacts
    Some dependencies could be POM files, which scalameta's ClassPathIndex
    (used by scalafix) cannot be loaded because it tries to open them using
    JarFile.
    
    So, let's use JarFile on them ourselves.

--- src/main/scala/io/github/evis/scalafix/maven/plugin/params/ProjectDependenciesParam.scala
+++ src/main/scala/io/github/evis/scalafix/maven/plugin/params/ProjectDependenciesParam.scala
@@ -1,5 +1,9 @@
 package io.github.evis.scalafix.maven.plugin.params
 
+import java.util.jar.JarFile
+
+import scala.util.Try
+
 import org.apache.maven.artifact.Artifact
 
 object ProjectDependenciesParam {
@@ -10,7 +14,10 @@ object ProjectDependenciesParam {
       deps.find { dep =>
         dep.getGroupId == "org.scala-lang" && dep.getArtifactId == "scala-library"
       }
-    _.withClasspath(deps.map(_.getFile.toPath))
-      .withScalaVersion(scalaLibrary.map(_.getVersion))
+    _.withClasspath(deps.flatMap { dep =>
+      val file = dep.getFile
+      val ok = !file.isFile || Try(new JarFile(file)).isSuccess
+      if (ok) Some(file.toPath) else None
+    }).withScalaVersion(scalaLibrary.map(_.getVersion))
   }
 }
