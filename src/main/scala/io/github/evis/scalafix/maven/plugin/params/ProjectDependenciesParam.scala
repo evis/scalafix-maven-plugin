07373d4155cd8bac2773df1bdc8d2c0f04f9b0ef
Evgeny Veretennikov <evg.veretennikov@gmail.com>
2024-05-05 14:39:49 +0300

    basic support for Scala 3 + update Scala, Scalafix #143

--- src/main/scala/io/github/evis/scalafix/maven/plugin/params/ProjectDependenciesParam.scala
+++ src/main/scala/io/github/evis/scalafix/maven/plugin/params/ProjectDependenciesParam.scala
@@ -12,7 +12,7 @@ object ProjectDependenciesParam {
     val deps = projectDependencies.toList
     val scalaLibrary =
       deps.find { dep =>
-        dep.getGroupId == "org.scala-lang" && dep.getArtifactId == "scala-library"
+        dep.getGroupId == "org.scala-lang" && (dep.getArtifactId == "scala-library" || dep.getArtifactId == "scala3-library_3")
       }
     _.withClasspath(deps.flatMap { dep =>
       /* Some dependencies could be POM files, which scalameta's ClassPathIndex
